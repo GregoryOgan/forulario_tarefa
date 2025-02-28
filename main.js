488d62eec310c1096492ad3915e1d87cbe6bb8af
gregoryogan <gregoryogan@gmail.com>
2025-02-28 16:41:31 -0300

    committed

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const form = document.getElementById('form-calcular')
 
 function validaNumero(numeroA,numeroB){
-    return numeroA.value < numeroB.value;  
+    return numeroA < numeroB;  
 }
 
 form.addEventListener('submit',function(e) {
     e.preventDefault();
+
+    const numeroASubmit = document.getElementById('number-a');
+    const numeroBSubmit = document.getElementById('number-b');
+
+    if (validaNumero(numeroASubmit.value,numeroBSubmit.value)) {
+        alert('O numero digitado é válido!');
+    }else{    
+        alert('O numero é inválido!');
+    }
 })
 
-const numeroASubmit = document.getElementById('number-a');
-const numeroBSubmit = document.getElementById('number-b');
 
-if (validaNumero(numeroASubmit,numeroBSubmit)) {
-    alert('O numero digitado é válido!');
-}else{
-
-    alert('O numero é inválido!');
-}
 
 console.log(form);
